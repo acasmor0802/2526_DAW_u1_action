9c6c123718b7fede567e597fe5e1b460bc7e3f7c
acasmor0802 <cmalfonso@iesrafaelalberti.es>
2025-10-08 21:44:05 +0200

    automatizacion de documentacion con pdoc en html y txt

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 def saludo(nombre: str) -> str:
+    """
+    Saluda a una persona por su nombre.
+
+    Args:
+        nombre (str): Nombre de la persona.
+
+    Returns:
+        str: Saludo con nombre personalizado.
+    """
     return f"Hola, {nombre}!" # Cambio para forzar error. Para que no falle: f"Hola, {nombre}!"
